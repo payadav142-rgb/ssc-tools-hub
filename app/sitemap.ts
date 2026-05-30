7d9cb4f3573dc2569dd330f1a5c7037a66b861a9
Pawan <payadav142@gmail.com>
2026-05-30 15:18:23 +0530

    fixed sitemap urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,122 +1,35 @@
 import { MetadataRoute } from "next";
 
+const BASE_URL =
+  "https://ssc-tools-hub.vercel.app";
+
 export default function sitemap():
 MetadataRoute.Sitemap {
 
-  return [
+  const routes = [
 
-    {
-      url:
-        "http://localhost:3000",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/tools",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/resources",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/pyqs",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/dashboard",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-cgl-salary",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-cgl-cutoff",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-cgl-books",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-cgl-exam-date",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-cgl-notification",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-cgl-job-profile",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-cgl-post-preference",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-chsl-salary",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-mts-salary",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-gd-salary",
-      lastModified:
-        new Date(),
-    },
-
-    {
-      url:
-        "http://localhost:3000/ssc-cgl-admit-card",
-      lastModified:
-        new Date(),
-    },
+    "",
+    "/tools",
+    "/resources",
+    "/pyqs",
+    "/dashboard",
+    "/ssc-cgl-salary",
+    "/ssc-cgl-cutoff",
+    "/ssc-cgl-books",
+    "/ssc-cgl-exam-date",
+    "/ssc-cgl-notification",
+    "/ssc-cgl-job-profile",
+    "/ssc-cgl-post-preference",
+    "/ssc-chsl-salary",
+    "/ssc-mts-salary",
+    "/ssc-gd-salary",
+    "/ssc-cgl-admit-card",
 
   ];
+
+  return routes.map((route) => ({
+    url: `${BASE_URL}${route}`,
+    lastModified: new Date(),
+  }));
 
 }
